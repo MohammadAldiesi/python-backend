5c6655671bde7977e241bdd9d0867dfaf525c78f
Mohammad Aldisi <mohammadaldisi@training-pc13-linux.scoiumtech.local>
2025-08-05 18:22:03 +0300

    day4

--- day4/Day4Exercise.py
+++ day4/Day4Exercise.py
@@ -1,8 +1,22 @@
 data = [10, 40, 32, 67, 23, 67, 89, 89]
 unique_data = list(set(data))
-unique_data.sort()
-second_largest = unique_data[-2]
-print("Second largest number:", second_largest)
+
+def second_largest(x):
+    if len(x) < 2:
+        return None
+
+    first, second = x[0], x[1]
+    if first < second:
+        first, second = second, first
+
+    for n in x[2:]:
+        if n > first:
+            first, second = n, first
+        elif n > second and n != first:
+            second = n
+    return second
+
+print("Second largest number:", second_largest(unique_data))
 
 dict1 = {'a': 1, 'b': 2}
 dict2 = {'b': 3, 'c': 4}
